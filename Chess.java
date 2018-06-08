67750fbf7495f73a9ece97cdca323ed1bf64bae0
StewStew1 <40000002+StewStew1@users.noreply.github.com>
2018-06-08 12:57:21 -0400

    Update Chess.java
    detects when king is captured
    probably gonna work on detecting stalemate now

--- Chess.java
+++ Chess.java
@@ -33,7 +33,7 @@ public class Chess extends JPanel{
 	 */
 	public static byte[][] board = new byte [8][8];
 	public static int startX, startY, endX, endY;
-	public static boolean turn = true, pvp = true, playerSide = true, whiteCastleKing = true, whiteCastleQueen = true, blackCastleKing = true, blackCastleQueen = true;
+	public static boolean turn = true, pvp = true, playerSide = true, win = false, whiteCastleKing = true, whiteCastleQueen = true, blackCastleKing = true, blackCastleQueen = true;
 	public static ArrayList <Piece> piecesB = new ArrayList <Piece> ();
 	public static ArrayList <Piece> piecesW = new ArrayList <Piece> ();
 	public static ArrayList <Move> legalMoves = new ArrayList <Move> ();
@@ -52,6 +52,7 @@ public class Chess extends JPanel{
 	 * resets all the pieces to amount of starting pieces
 	 */
 	public static void setBoard () {
+		win = false;
 		turn = true;
 		whiteCastleKing = true;
 		whiteCastleQueen = true;
@@ -102,6 +103,7 @@ public class Chess extends JPanel{
 		piecesB.add(new Piece ((byte)-2));
 	}
 
+	// Formats the moves into chess into algebraic chess notation
 	public static void showMoves () {
 		for (int a = 0; a < movesPlayed.size(); a++)
 			if (a%2==0)
@@ -157,12 +159,20 @@ public class Chess extends JPanel{
 		bufferStrat.show();
 	}
 
+	public static void makeMove (Move m) {
+		board[m.endX][m.endY] = board [m.startX][m.startY]; // moves pieces on the board
+		board [m.startX][m.startY] = 0;
+	}
+
 	/*
 	 * makes move for white
 	 */
 	public static void moveWhite () {
-		Move.findLegalMoves (turn);
+		legalMoves.clear();
+		legalMoves = Move.findLegalMoves (turn, board);
 		if (!playerSide && !pvp) { //engine moves
+			currentMove = Engine.generateMove(legalMoves);
+			System.out.println(currentMove);
 			turn = false;
 		}
 		else { // player moves
@@ -176,31 +186,22 @@ public class Chess extends JPanel{
 				}
 			}
 			if (temp) {
-				if (board [startX][startY] == 6 && endX == 6 && whiteCastleKing) { // white castling kingside
-					board [5][0] = 4;
-					board [7][0] = 0;
+				castle(board, currentMove);
+				if (board[endX][endY] < 0) {
+					for (int a = 0; a < piecesB.size(); a ++) {
+						if (piecesB.get(a).name == board[endX][endY]) {
+							piecesB.remove(a);
+							break;
+						}
+					}
 				}
-				else if (board [startX][startY] == 6 && endX == 2 && whiteCastleQueen) { // white castling queenside
-					board [3][0] = 4;
-					board [0][0] = 0;
-				}
-				if (board[startX][startY] == 4 && startX == 0) // this checks to see if a rook or king moved and if so, prevents castling
-					whiteCastleQueen = false;
-				else if (board[startX][startY] == 4 && startX == 7)
-					whiteCastleKing = false;	
-				else if (board[startX][startY] == 6) {
-					whiteCastleKing = false;
-					whiteCastleQueen = false;
-				}
-				board[endX][endY] = board [startX][startY]; // moves pieces on the board
-				board [startX][startY] = 0;
+				makeMove (currentMove);
 				if (endY == 7 && board[endX][endY] == 1) { // Pawn Promotion when pawn reaches end of the board
 					byte choice;
 					Object[] options1 = {	"Knight",
-											"Bishop",
-											"Rook",
-											"Queen"};
-
+							"Bishop",
+							"Rook",
+					"Queen"};
 					choice = (byte) JOptionPane.showOptionDialog(null,
 							null, 
 							"Promotion",
@@ -211,9 +212,27 @@ public class Chess extends JPanel{
 							null);
 					choice += 2;
 					board[endX][endY] = choice;
+					char name;
+					if (choice == 5)		name = 'Q';
+					else if (choice == 2)	name = 'N';
+					else if (choice == 4)	name = 'R';
+					else					name = 'B';
+					movesPlayed.get(movesPlayed.size()-1).moveName += "=" + name;
+					piecesW.remove(0);
+					piecesW.add(new Piece ((byte) choice));
 				}
+				legalMovesNextPlayer.clear();
+				legalMovesNextPlayer = Move.findLegalMoves(!turn, board);
+				temp = false;
+				for (int a = 0; a < piecesB.size(); a ++)
+					if (piecesB.get(a).name == -6)
+						temp = true;
+				if (!temp)
+					win = true;
 				turn = false;
-				legalMoves.clear();
+				if  (!pvp)
+					moveBlack ();
+				//showMoves();
 			}
 		}
 	}
@@ -222,8 +241,11 @@ public class Chess extends JPanel{
 	 * makes move for black
 	 */
 	public static void moveBlack () {
-		Move.findLegalMoves (turn);
+		legalMoves.clear();
+		legalMoves = Move.findLegalMoves (turn, board);
 		if (playerSide && !pvp) { //engine moves
+			currentMove = Engine.generateMove(legalMoves);
+			makeMove (currentMove);
 			turn = true;
 		}
 		else { // player moves
@@ -237,31 +259,23 @@ public class Chess extends JPanel{
 				}
 			}
 			if (temp) {
-				if (board [startX][startY] == -6 && endX == 6 && blackCastleKing) { // white castling kingside
-					board [5][7] = -4;
-					board [7][7] = 0;
-				}
-				else if (board [startX][startY] == -6 && endX == 2 && blackCastleQueen) { // white castling queenside
-					board [3][7] = -4;
-					board [0][7] = 0;
-				}
-				if (board[startX][startY] == -4 && startX == 0) // this checks to see if a rook or king moved and if so, prevents castling
-					blackCastleQueen = false;
-				else if (board[startX][startY] == -4 && startX == 7)
-					blackCastleKing = false;	
-				else if (board[startX][startY] == -6) {
-					blackCastleKing = false;
-					blackCastleQueen = false;
+				castle(board, currentMove);
+				if (board[endX][endY] > 0) {
+					for (int a = 0; a < piecesB.size(); a ++) {
+						if (piecesW.get(a).name == board[endX][endY]) {
+							piecesW.remove(a);
+							break;
+						}
+					}
 				}
 				board[endX][endY] = board [startX][startY]; // moves pieces on the board
 				board [startX][startY] = 0;
 				if (endY == 0 && board[endX][endY] == -1) {
 					byte choice;
 					Object[] options1 = {	"Knight",
-											"Bishop",
-											"Rook",
-											"Queen"};
-
+							"Bishop",
+							"Rook",
+					"Queen"};
 					choice = (byte) JOptionPane.showOptionDialog(null,
 							null, 
 							"Promotion",
@@ -272,11 +286,69 @@ public class Chess extends JPanel{
 							null);
 					choice += 2;
 					board[endX][endY] = (byte) -choice;
+					char name;
+					if (choice == 5)		name = 'Q';
+					else if (choice == 2)	name = 'N';
+					else if (choice == 4)	name = 'R';
+					else					name = 'B';
+					movesPlayed.get(movesPlayed.size()-1).moveName += "=" + name;
+					piecesB.remove(0);
+					piecesB.add(new Piece ((byte) -choice));
 				}
+				legalMovesNextPlayer.clear();
+				legalMovesNextPlayer = Move.findLegalMoves(!turn, board);
+				temp = false;
+				for (int a = 0; a < piecesW.size(); a ++)
+					if (piecesW.get(a).name == 6)
+						temp = true;
+				if (!temp)
+					win = true;
 				turn = true;
-				legalMoves.clear();
+				if  (!pvp)
+					moveWhite ();
+				//showMoves();
 			}
 		}
+	}
+
+	public static void castle (byte[][] board, Move currentMove) {
+		if (turn) {
+			if (board [currentMove.startX][currentMove.startY] == 6 && currentMove.endX == 6 && whiteCastleKing) { // white castling kingside
+				board [5][0] = 4;
+				board [7][0] = 0;
+			}
+			else if (board [currentMove.startX][currentMove.startY] == 6 && currentMove.endX == 2 && whiteCastleQueen) { // white castling queenside
+				board [3][0] = 4;
+				board [0][0] = 0;
+			}
+			if (board[currentMove.startX][currentMove.startY] == 4 && currentMove.startX == 0) // this checks to see if a rook or king moved and if so, prevents castling
+				whiteCastleQueen = false;
+			else if (board[currentMove.startX][currentMove.startY] == 4 && currentMove.startX == 7)
+				whiteCastleKing = false;	
+			else if (board[currentMove.startX][currentMove.startY] == 6) {
+				whiteCastleKing = false;
+				whiteCastleQueen = false;
+			}
+		}
+		else {
+			if (board [currentMove.startX][currentMove.startY] == -6 && currentMove.endX == 6 && blackCastleKing) { // white castling kingside
+				board [5][7] = -4;
+				board [7][7] = 0;
+			}
+			else if (board [currentMove.startX][currentMove.startY] == -6 && currentMove.endX == 2 && blackCastleQueen) { // white castling queenside
+				board [3][7] = -4;
+				board [0][7] = 0;
+			}
+			if (board[currentMove.startX][currentMove.startY] == -4 && currentMove.startX == 0) // this checks to see if a rook or king moved and if so, prevents castling
+				blackCastleQueen = false;
+			else if (board[currentMove.startX][currentMove.startY] == -4 && currentMove.startX == 7)
+				blackCastleKing = false;	
+			else if (board[currentMove.startX][currentMove.startY] == -6) {
+				blackCastleKing = false;
+				blackCastleQueen = false;
+			}
+		}
+
 	}
 
 	/*
@@ -310,11 +382,26 @@ public class Chess extends JPanel{
 				endX = e.getX()/50-1;
 				endY = 8-e.getY()/50;
 				if (startX >= 0 && startX <=8 && startY >= 0 && startY <=8 && endX >= 0 && endX <=8 && endY >= 0 && endY <=8) { // checks to see if user clicked inside the board
-					if (turn)
-						moveWhite();
-					else
-						moveBlack();
+					if (!pvp && playerSide) {
+						moveWhite ();
+					}
+					else if (!playerSide)
+						moveBlack ();
+					else {
+						if (turn)
+							moveWhite();
+						else
+							moveBlack();
+					}
 					drawBoard();
+					if (win) {
+						if (!turn)
+							JOptionPane.showMessageDialog(Chess.frame, "Black has lost.");
+						else if (turn)
+							JOptionPane.showMessageDialog(Chess.frame, "White has lost.");
+						setBoard ();
+						drawBoard ();
+					}
 				}
 			}
 		});
