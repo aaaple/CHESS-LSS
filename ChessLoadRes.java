fcf23d9633faaa8f4bdc7f49bebbda6a3fd45451
StewStew1 <40000002+StewStew1@users.noreply.github.com>
2018-06-10 17:55:22 -0400

    Update ChessLoadRes.java

--- ChessLoadRes.java
+++ ChessLoadRes.java
@@ -1,26 +1,17 @@
 package ISU;
 
-/* 
- * This class loads all the resources
- */
-
+// This class loads all the resources
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+// This class loads all the images that will be used
 public class ChessLoadRes {
-
-	// this class basically loads in all our images
-	//it also loads in our music
-	//everything is static because we need all the resources at all times, and don't want to have to load in images after the game starts
 	public static BufferedImage wk,wq,wr,wb,wn,wp,bk,bq,br,bb,bn,bp,back,silk,silq,silr,silb,siln,silp,title;
-	/*
-	 * Description: loads assets
-	 * Arguments: none
-	 * Return Type: void
-	 */
+	
+	// creates BufferedImages
 	public static void loadAssets(){
 		bk    = loadImage ("src/ISU/res/chess_piece_black_king.png");
 		bq    = loadImage ("src/ISU/res/chess_piece_black_queen.png");
@@ -43,11 +34,8 @@ public class ChessLoadRes {
 		back  = loadImage ("src/ISU/res/back.png");
 		title = loadImage ("src/ISU/res/title.png");
 	}
-	/*
-	 * Description: method to load image
-	 * Arguments: none
-	 * Return Type: void
-	 */
+	
+	// Loads images from files to the BufferedImages
 	private static BufferedImage loadImage(String path)
 	{
 		try {
@@ -58,7 +46,5 @@ public class ChessLoadRes {
 			System.exit(1);
 		}
 		return null;
-
 	}
-	
 }
